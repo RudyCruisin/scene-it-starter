639da42c1be98d06415f989114fcdfdd1a297d12
Rudy Saavedra <RudyCruisin@outlook.com>
2020-08-27 21:58:35 -0400

    part 3 functional

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
+var moviedatasearch;
+
 $(document).ready(() => {
   // console.log("Ready")
+  $("#search-form").on("submit", function (e) {
+    e.preventDefault();
+    var searchfield = e.target[0].value;
+    var urlEncodedSearchString = encodeURIComponent(searchfield);
+    fetch("http://www.omdbapi.com/?apikey=8534d2a7&s=" + urlEncodedSearchString)
+      .then((response) => response.json())
+      .then((data) => {
+        moviedatasearch = data.Search;
+        renderMovie(data.Search);
+        console.log("two: " + moviedatasearch);
+        // console.log(data);
+      });
+  });
+
+  console.log("one: " + moviedatasearch);
+  // renderMovie(movieData);
 
   function renderMovie(movieArray) {
-    //         console.log("i'm here")
-    // console.log(movieArray)
-    // document
-    // .getElementById("search-form")
-    $("#search-form")
-      .on("submit", function (e) {
-        $(".movies-container").empty();
-        const movieHTML = movieArray.map((currentMovie) => {
-          // console.log(currentMovie.Title)
-          e.preventDefault();
-          var searchfield = e.target[0].value;
-          if (
-            (kmpSearch(
-              searchfield.toLowerCase(),
-              currentMovie.Title.toLowerCase()
-            ) !=
-              -1) ===
-              true ||
-            (kmpSearch(
-              searchfield.toLowerCase(),
-              currentMovie.Year.toLowerCase()
-            ) !=
-              -1) ===
-              true
-          ) {
-            $(".movies-container").append(`
+    $(".movies-container").empty();
+    const movieHTML = movieArray.map((currentMovie) => {
+      // console.log(currentMovie.Title)
+      $(".movies-container").append(`
                         <div class = "movie">
                             <img class = "movie-image" src = "${currentMovie.Poster}" />
                             <div class = "movie-title-date">
@@ -37,27 +33,24 @@ $(document).ready(() => {
                                 </div>
                             </div>
                             <div class = "card-buttons">
-                              <button class = "add-button" onClick = saveToWatchList("${currentMovie.imdbID}") type = "button">ADD</button>
+                              <button class = "add-button" onClick = saveToWatchList('${currentMovie.imdbID}') type = "button">ADD</button>
                               <button class = "movie-button" type = "button"><a class = "imdb" href= "https://www.imdb.com/title/${currentMovie.imdbID}">IMDB</a></button>
                             </div>
                         </div>
                 `);
-          };
-
-        });
-      });
+    });
     // console.log(movieHTML.join(' '))
     // $('.movies-container').html(movieHTML.join(' '))
-  };
-  renderMovie(movieData);
+  }
+  // renderMovie();
 });
 
 function saveToWatchList(imdbID) {
   // console.log("happy");
-  var movie = movieData.find((currentMovie) => {
+  var movie = moviedatasearch.find((currentMovie) => {
     return currentMovie.imdbID == imdbID;
   });
-  var watchlistJSON = localStorage.getItem('watchlist');
+  var watchlistJSON = localStorage.getItem("watchlist");
   var watchlist = JSON.parse(watchlistJSON);
   // console.log(watchlist);
   if (watchlist == null) {
@@ -66,28 +59,4 @@ function saveToWatchList(imdbID) {
   watchlist.push(movie);
   watchlistJSON = JSON.stringify(watchlist);
   localStorage.setItem("watchlist", watchlistJSON);
-}
-
-function kmpSearch(pattern, text) {
-  if (pattern.length == 0) return 0; // Immediate match
-
-  // Compute longest suffix-prefix table
-  var lsp = [0]; // Base case
-  for (var i = 1; i < pattern.length; i++) {
-    var j = lsp[i - 1]; // Start by assuming we're extending the previous LSP
-    while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) j = lsp[j - 1];
-    if (pattern.charAt(i) == pattern.charAt(j)) j++;
-    lsp.push(j);
-  }
-
-  // Walk through text string
-  var j = 0; // Number of chars matched in pattern
-  for (var i = 0; i < text.length; i++) {
-    while (j > 0 && text.charAt(i) != pattern.charAt(j)) j = lsp[j - 1]; // Fall back in the pattern
-    if (text.charAt(i) == pattern.charAt(j)) {
-      j++; // Next char matched, increment position
-      if (j == pattern.length) return i - (j - 1);
-    }
-  }
-  return -1; // Not found
 }
